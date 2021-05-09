5f52069b2f1fa092273f40303519acd2b97f159a
Basak ulcay <55965449+basakulcay@users.noreply.github.com>
2021-05-09 15:26:26 -0700

    Update medical_insurance.py

--- medical_insurance.py
+++ medical_insurance.py
@@ -1,46 +1,17 @@
-# Function to estimate insurance cost:
-def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
-  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
-  return estimated_cost
- 
-# Estimate Maria's insurance cost
-maria_insurance_cost = estimate_insurance_cost(name = "Maria", age = 31, sex = 0, bmi = 23.1, num_of_children = 1, smoker = 0)
-
-# Estimate Rohan's insurance cost
-rohan_insurance_cost = estimate_insurance_cost(name = 
-"Rohan", age = 25, sex = 1, bmi = 28.5, num_of_children = 3, smoker = 0)
-
-# Estimate Valentina's insurance cost
-valentina_insurance_cost = estimate_insurance_cost(name = "Valentina", age = 53, sex = 0, bmi = 31.4, num_of_children = 0, smoker = 1)
+names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily", "Nikita", "Paul"]
+insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
 
 # Add your code here
-names=["Maria","Rohan","Valentina"]
-insurance_cost=[4150.0,5320.0,35210.0]
+names.append("Priscilla")
+insurance_costs.append(8320.0)
 
-insurance_data = list(zip(names,insurance_cost))
-print("Here is the actual insurance cost data: ",insurance_data)
+medical_records=list(zip(insurance_costs,names))
 
-estimated_insurance_data=[]
+print(medical_records)
 
-estimated_insurance_data.append(("Maria",maria_insurance_cost))
-estimated_insurance_data.append(("Rohan",rohan_insurance_cost))
-estimated_insurance_data.append(("Valentina",valentina_insurance_cost))
+num_medical_records = len(medical_records)
+print("There are ",num_medical_records,"medical reports.")
 
-print("Here is the estimated insurance cost data: ",estimated_insurance_data)
 
-estimated_insurance_cost=[]
-estimated_insurance_cost.append(maria_insurance_cost)
-estimated_insurance_cost.append(rohan_insurance_cost)
-estimated_insurance_cost.append(valentina_insurance_cost)
 
-insurance_cost_difference = []
-  
-for i in range(len(estimated_insurance_cost)):
-    if estimated_insurance_cost[i] > insurance_cost[i]:
-        insurance_cost_difference.append(estimated_insurance_cost[i] - insurance_cost[i])
-    else:
-        insurance_cost_difference.append(estimated_insurance_cost[i])
-
-print("The difference is ",insurance_cost_difference)
 
