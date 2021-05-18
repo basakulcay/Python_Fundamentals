a99194c346e202c5046e615235c6a85ac9627a40
Basak ulcay <55965449+basakulcay@users.noreply.github.com>
2021-05-17 19:00:49 -0700

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,29 @@
+class Student: 
+  pass
+
+  def __init__(self,name,year):
+    self.name = name
+    self.year = year
+    self.grades = []
+
+  def add_grade(self,grade):
+    if type(grade) is Grade:
+      self.grades.append(grade)
+
+  def get_average(self,average):
+    self.average = average 
+
+roger = Student("Roger van der Weyden",10)
+sandro = Student("Sandro Botticelli",12)
+pieter = Student("Pieter Bruegel the Elder",8)
+
+class Grade:
+  minimum_passing=65
+  
+  def __init__(self,score):
+    self.score = score
+  
+  
+
+pieter.add_grade(Grade(100))
+
